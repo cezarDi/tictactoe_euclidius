81346dc0618cf070cda70a210c2ee2aa58bac70d
Tim <perfodio@yandex.ru>
2024-11-25 10:02:53 +0300

    Added ablility to get move position

--- src/player.cpp
+++ src/player.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "player.h"
 
 #include <toml.hpp>
@@ -7,4 +9,18 @@ void Player::parse_config(const std::string& config_file) {
 
     login = toml::find<std::string>(data, "login").c_str();
     password = toml::find<std::string>(data, "password").c_str();
+}
+
+
+std::pair<char, char> Player::get_move_position() {
+    std::pair<char, char> coords;
+    do {
+        std::cout << "Enter the coords where you want to place your figure:" << std::endl;
+        std::cin >> coords.first >> coords.second;
+    } while (coords.first < '1' || coords.first > '4' || coords.second < '1' || coords.second > '4');
+
+    coords.first -= 1;
+    coords.second -= 1;
+
+    return coords;
 }
--- src/player.h
+++ src/player.h
@@ -17,6 +17,13 @@ public:
     std::string get_password() {
         return password;
     }
+
+    /*
+     * Gets the pair (x, y) to move
+     * 1 <= x, y <= 4
+     */
+    std::pair<char, char> get_move_position();
+
 private:
     void parse_config(const std::string& config_file);
     bool order;
